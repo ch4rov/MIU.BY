71bfaabe00bb47376c32173fe9dbb82d2232e754
CharovPlay <ilyagoncharov14@gmail.com>
2022-02-08 22:20:50 +0300

    Update Goncharov, lab-01, task 1.c

--- 1 year, 1 sem/OAiP/LABS/Goncharov, lab-01, task 1.c
+++ 1 year, 1 sem/OAiP/LABS/Goncharov, lab-01, task 1.c
@@ -1,16 +1,16 @@
-#include <stdio>
+// 1. Дан диаметр окружности d. Найти ее длину L = πd. Взять π = 3.14.
+#include <stdio.h>
 #include <locale.h>
 
 int main()
 {
- int d;
- float L, Pi = 3.14;
- setlocale(0, "rus"); //подключаем кириллицу (русский язык)
- printf("Введите диаметр окружности: ");
- scanf_s("%d", &d);
- L = Pi * d;
- printf("Длина окружности = %.2f\n", L);
- printf("Vopvet.Ru - учеба доступнее\n");
+ int d; // Диаметр окружности
+ float L, P = 3.14; // Длина окружности и число пи (3.14)
+ setlocale(0, "rus"); // Эта параша как всегда не работает, поэтому напишу транслитом.
+ printf("Enter diameter of circle: ");
+ scanf("%d", &d);
+ L = P * d;
+ printf("Dlina okruzhnosti = %.2f\n", L);
  getchar();
  getchar();
  return 0;
